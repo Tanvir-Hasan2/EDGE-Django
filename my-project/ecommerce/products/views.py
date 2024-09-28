0d24c843aaee8c56123354e9d66500c0692730eb
tanvir-hasan <tanvir8661@gmail.com>
2024-09-28 12:33:38 +0600

    data base migratation and initialization and encode decode on the format of json

--- my-project/ecommerce/products/views.py
+++ my-project/ecommerce/products/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.views import APIView
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hello, world!!")
+
+class ProductView(APIView):
+    def post(self,request):
+        pass
+
+
+
+    def get(self,request):
+        pass
